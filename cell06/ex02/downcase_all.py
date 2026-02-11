c58bef4b2fca0716d7c64e3fa77672397b4a41d9
KhetaranSara <65050101@kmitl.ac.th>
2026-02-11 20:15:16 +0700

    downcase

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,11 @@
+import sys
+
+def downcase_it(text):
+    return text.lower()
+
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for arg in sys.argv[1:]:
+        result = downcase_it(arg)
+        print(result)
